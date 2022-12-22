ef63fb7de4d7e930304be9f17d3b16dedb5fa8f7
DHing1994 <d_hing@hotmail.com>
2022-12-22 09:55:09 +0800

    Added Comments

--- PyBank/budgetmain.py
+++ PyBank/budgetmain.py
@@ -1,7 +1,9 @@
 
+#import libraries
 import os
 import csv
 
+#define variables for following code
 Months=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 Count=0
 Total=0
@@ -12,44 +14,60 @@ Min=0
 TotalChange=0
 FirstV=0
 
-
-
-
+#set filepath to find source document
 csvpath=os.path.join('.','Resources','budget_data.csv')
 with open(csvpath, encoding="utf") as csvfile:
     bankreader=csv.reader(csvfile,delimiter=",")
+    
+    #check to skip header row
     for row in bankreader:
         if row[1]=="Profit/Losses":
             continue
+        #storing value of row
         Current=int(row[1])
+
+        #storing first value of source
         if FirstV==0:
             FirstV=int(row[1])
+
+        #Counting number of months
         if (row[0][0:3]) in Months:
             Count=Count+1
             Total=Current+Total
             Change=Current-Old
 
+        #Setting a max value and date
         if Change>Max:
             Max=Change
+            MaxDate=str(row[0][0:6])
+
+        #Setting a min value and date
         if Change<Min:
             Min=Change
+            MinDate=str(row[0][0:6])
 
+        #shifting value of Old for month count function
         Old=Current
 
+    #calculate average change
     AvgC=round((Current-FirstV)/(Count-1),2)
 
+    #print output
+    print("Financial Analysis \n")
+    print("----------------------------------------------\n")
     print("There are "+str(Count)+" months")
     print("The Profit/Loss was $"+str(Total))
     print("The average change was $" + str(AvgC))
-    print("The greatest gain was $"+str(Max))
-    print("The greatest loss was $"+str(-Min))
+    print("The greatest gain was $"+str(Max) + " on the " + MaxDate)
+    print("The greatest loss was $"+str(-Min) + " on the " + MinDate)
 
-    with open ('Election.txt' ,  "w") as f:
+    #write output to text doc in analysis
+    with open ('./analysis/Election.txt' ,  "w") as f:
         f.write("There are "+str(Count)+" months" + "\n" + 
         "The Profit/Loss was $" + str(Total) + "\n" +
         "The average change was $" + str(AvgC) +"\n" +
-        "The greatest gain was $"+str(Max) + "\n" +
-        "The greatest loss was $"+str(-Min)) 
+        "The greatest gain was $"+str(Max) + " on the " + MaxDate + "\n" +
+        "The greatest loss was $"+str(-Min) + " on the " + MinDate)
 
         
 
--- PyPoll/pollmain.py
+++ PyPoll/pollmain.py
@@ -1,13 +1,19 @@
+#importing libraries
 import os
 import csv
 
+#Total vote is running count for votes
 TotalVotes=0
+#Candidates is a dictionary to track candidate votes
 Candidates={}
 
+#set filepath to find source document
 csvpath=os.path.join('.','Resources','election_data.csv')
 with open(csvpath, encoding="utf") as csvfile:
-    pollreader=csv.reader(csvfile,delimiter=",")   
+    pollreader=csv.reader(csvfile,delimiter=",")
     pollreader.__next__()
+
+    #check each row for new candidate entries, then add vote to their dictionary value
     for row in pollreader:
         TotalVotes+=1
         if row[2] not in Candidates:
@@ -15,18 +21,24 @@ with open(csvpath, encoding="utf") as csvfile:
         else:
             Candidates[row[2]]+=1
 
+#print header output
 print("Election Results")
-
 print("-----------------------------------------------")
+
+#print Total vote count
 print(str(TotalVotes)+" votes cast")
 print("-----------------------------------------------")
+
+#print candidate names and their vote counts
 for key,value in Candidates.items():
     print(key + ": " + str(round(((value/TotalVotes)*100),3)) + "% (" + str(value) + ")")
 print("-----------------------------------------------")
 
+#print winner
 print("Winner is " + max(Candidates, key=Candidates.get))
 
-with open('Poll.txt' , 'w') as f:
+#write the results into a text doc in the analysis folder
+with open('./analysis/Poll.txt' , 'w') as f:
     f.write(str(TotalVotes)+ ' votes cast' + "\n")
     for key,value in Candidates.items():
         f.write(key + ": " + str(round(((value/TotalVotes)*100),3)) + "% (" + str(value) + ")" +"\n" +
